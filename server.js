405aec8758274cd35f76adc263c4175e7145cf9c
Rose Koenings <rnkings.05@gmail.com>
2016-11-12 14:54:38 -0600

    wrote out my ng-app=songBird to bootstrap my app with angular and I wrote an example controller in my app.js called tuneSubmitController and linked it in my index.html and I added a submit directive, I also added a ng-repeat above where I want my results to appear that is connected to my ng-show directive called songResults and added expressions to show the art, song name and preview

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
 "use strict";
+
+var express = require("express");
+var fs = require('file-system');
+var app = express();
+
+// ROOT DIRECTORY
+app.use('/', express.static(__dirname + '/public'));
+
+
+
+
+
+
+
+
+
+app.listen(3002, function () {
+  console.log('See this website at localhost:3002');
+});
