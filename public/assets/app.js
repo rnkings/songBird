467703ad6dd7ef8952173cea505d164a8f2bc234
Rose Koenings <rnkings.05@gmail.com>
2016-11-12 23:12:12 -0600

    completed pagination

--- public/assets/app.js
+++ public/assets/app.js
@@ -7,7 +7,11 @@ app.config(function($sceProvider) {
 	$sceProvider.enabled(false);
 });
 
-var tuneSubmitController = function($scope, $http, $log){
+var tuneSubmitController = function($scope, $http){
+	$scope.songResults = [];
+	$scope.currentPage = 1;
+	$scope.perPage = 5;
+
 	var onResultsReturned = function(response){
 		$scope.data = response.data;
 		$scope.songResults = response.data.results;
@@ -25,8 +29,62 @@ var tuneSubmitController = function($scope, $http, $log){
 	  	}
 	  }).then(onResultsReturned, onError)
 	}
+
+	/**
+	 * Pagination Methods
+	 **/
+
+	//Creates an array with all pages as numbers
+	$scope.getPageArray = function () {
+		var pages = [];
+		var maxPage = $scope.getMaxPage();
+		for (var i = 1; i <= maxPage; i++) {
+			pages.push(i);
+		}
+		return pages;
+	}
+
+	//Retrieves the max number of pages needed.
+	$scope.getMaxPage = function () {
+		return (Math.ceil($scope.songResults.length / $scope.perPage));
+	};
+
+	//Retrieves the data to display based on the current page.
+	$scope.getPageData = function () {
+		//We grab a start index, based on the current page and items per page.
+		//i.e. currentPage is 1, this turns into (0 * 5), which is zero
+		var start = ($scope.currentPage - 1) * $scope.perPage;
+		//We then add the number of items per page to get end point
+		var end = start + $scope.perPage;
+		//We use slice to grab items from start to end
+		return $scope.songResults.slice(start, end);
+	};
+
+	//Changes our page to a new page.
+	$scope.changePage = function (newPage) {
+		$scope.currentPage = newPage;
+	};
+
+	//Increments our page by one.
+	$scope.nextPage = function () {
+		$scope.changePage($scope.currentPage + 1);
+	};
+
+	//Decrements our page by one.
+	$scope.previousPage = function () {
+		$scope.changePage($scope.currentPage - 1);
+	};
 };
 
 app.controller('tuneSubmitController', ['$scope', '$http', tuneSubmitController]);
+
+
+//Pagination
+
+
+
+
+
+
 
 
